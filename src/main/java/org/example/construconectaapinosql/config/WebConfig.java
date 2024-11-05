fcfe27d0b0d5dbe3913ec35bd3fa83b1383834d7
kauarsantoss <kaua.santos@germinare.org.br>
2024-11-05 10:32:10 -0300

    Alterando mapeamento de CORS para permitir o acesso da api em qualquer url

--- src/main/java/org/example/construconectaapinosql/config/WebConfig.java
+++ src/main/java/org/example/construconectaapinosql/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500") // Permitir apenas esta origem
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedOrigins("*")
+                .allowedMethods("*")
                 .allowCredentials(true);
     }
 }
